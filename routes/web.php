fa5b79d2d91fbc86985b0f63bbe1093cc0a7dd90
mmockelyn <mmockelyn@gmail.com>
2021-06-03 00:23:13 +0200

    UI Principal Page de Login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use UniSharp\LaravelFilemanager\Lfm;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::group(['prefix' => 'files', 'middleware' => 'auth'], function () {
+    Lfm::routes();
+});
+
+require __DIR__.'/auth.php';
