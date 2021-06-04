0947085f01a6ae671fe2c0d45d568d5bd0cf2841
mmockelyn <mmockelyn@gmail.com>
2021-06-04 21:38:03 +0200

    - UI aside.blade.php - UI header.blade.php - UI dashboard.blade.php - UI Bread & Toolbar

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -5,22 +5,9 @@
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1">
             <span class="text-muted fw-bold me-1">2021©</span>
-            <a href="" target="_blank" class="text-gray-800 text-hover-primary">Keenthemes</a>
+            <a href="" target="_blank" class="text-gray-800 text-hover-primary">Transportfever France</a>
         </div>
         <!--end::Copyright-->
-        <!--begin::Menu-->
-        <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
-            <li class="menu-item">
-                <a href="https://keenthemes.com/faqs" target="_blank" class="menu-link px-2">About</a>
-            </li>
-            <li class="menu-item">
-                <a href="https://keenthemes.com/support" target="_blank" class="menu-link px-2">Support</a>
-            </li>
-            <li class="menu-item">
-                <a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
-            </li>
-        </ul>
-        <!--end::Menu-->
     </div>
     <!--end::Container-->
 </div>
